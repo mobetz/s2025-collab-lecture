7164a8a6bbc50fab944747561d3d1005215715b0
Matthew Obetz <mobetz@gmail.com>
2025-05-07 11:16:19 -0400

    notes about adding remotes

--- Main.java
+++ Main.java
@@ -37,6 +37,42 @@ public class Main {
 
         Once we do this, we can see all the changes in our log with `git log` and review who made them, and add future
         commits exactly the way we always have.
+
+
+
+        However... when we make a repository with git init, it's not associated with any remote copy. What this means is
+        I do not inherently have the ability to run a git push:
+
+
+        $ git push
+            fatal: No configured push destination.
+            Either specify the URL from the command-line or configure a remote repository using
+
+                git remote add <name> <url>
+
+            and then push using the remote name
+
+                git push <name>
+
+
+        If we do want to take advantage of the remote 'cloud storage' features of git then we need to define some Remote
+        repository that will store the copy of our git history on the internet. Services like GitHub, GitLab, Forgero, and
+        BitBucket are all just websites that have the tools to set up a URL that we can point to for our git repos.
+
+
+        For any of these remote repos, the first step of using them is logging in to a user account. On the site once you
+        are logged in, you can create a remote copy of the repo by clicking the New Repository button, and this will let you
+        create a remote name for your project, and then you can set the URL that github gives you using the `git remote` command.
+
+        To associate our local copy of the code with the URL given, we use git remote:
+
+                git remote add <name_of_repo> <URL>
+
+        Note: only files and changes that are part of a commit get pushed when you use the git push command. So we need to make
+        sure to save our files and check `git status` to see if there are ever any pending changes that haven't been committed
+        before pushing.
+
+
         */
     }
 }
